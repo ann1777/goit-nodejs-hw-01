da9f3f69f6b8e93331765d0757338f616424573a
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-07-10 13:17:36 +0300

    Added screenshots to the REAADME.md

--- contacts.js
+++ contacts.js
@@ -5,52 +5,58 @@ import { nanoid } from "nanoid";
 const contactsPath = path.resolve("contacts", "contacts.json");
 
 export const getAllContacts = async () => {
-  const contacts = await fs
-    .readFile(contactsPath)
-    .then((data) => {
-      const parsed = JSON.parse(data);
-      console.table(parsed);
-    })
-    .catch((err) => console.log(err.message));
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const parsed = JSON.parse(data);
+    console.table(parsed);
+  } catch (err) {
+    console.log("Error:", err.message);
+  }
 };
 
-export const getContactById = async (Id) => {
-  const data = await fs
-    .readFile(contactsPath)
-    .then((data) => {
-      const parsed = JSON.parse(data);
-      const contact = parsed.find((contact) => contact.id === Id);
-      return console.log(contact) || null;
-    })
-    .catch((err) => console.log(err.message));
-};
-
-export const removeContact = (id) => {
-  fs.readFile(contactsPath)
-    .then((data) => {
-      const contacts = JSON.parse(data);
-      if (contacts.find((contact) => contact.id === id)) {
-        const contact = contacts.find((contact) => contact.id === id);
-        console.table(contact);
-      }
+export const getContactById = async (id) => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((contact) => contact.id === id);
+    if (contact) {
+      console.table(contact);
+    } else {
       console.log(null);
-      const result = contacts.filter((contact) => contact.id !== id);
-      fs.writeFile(contactsPath, JSON.stringify(result, null, 2));
-    })
-    .catch((err) => console.log(err.message));
+    }
+  } catch (err) {
+    console.log("Error:", err.message);
+  }
+};
+
+export const removeContact = async (id) => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((contact) => contact.id === id);
+    if (contact) {
+      console.table(contact);
+    } else {
+      console.log(null);
+    }
+    const result = contacts.filter((contact) => contact.id !== id);
+    await fs.writeFile(contactsPath, JSON.stringify(result, null, 2));
+  } catch (err) {
+    console.log("Error:", err.message);
+  }
 };
 
 export const addNewContact = async (name, email, phone) => {
-  const contact = [{ id: nanoid(), name, email, phone }];
-  fs.readFile(contactsPath)
-    .then((data) => {
-      const parsed = JSON.parse(data);
-      return console.table(parsed.concat(contact));
-    })
-    .then((result) =>
-      fs.writeFile(contactsPath, JSON.stringify(result, null, 2))
-    )
-    .catch((err) => console.log(err.message));
+  try {
+    const contact = { id: nanoid(), name, email, phone };
+    const data = await fs.readFile(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
+    const updatedContacts = contacts.concat(contact);
+    console.table(updatedContacts);
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+  } catch (err) {
+    console.log("Error:", err.message);
+  }
 };
 
 export default {
